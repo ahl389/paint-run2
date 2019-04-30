f41e47d52c185886623ff0e84d5a7e2ac7d5965f
Ashley Boucher <ashleyhlivingston@gmail.com>
2019-04-29 17:22:42 -0700

    Updates

---
+++ precache-manifest.26ed899682d1335a650dc0110dc4f767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9881e2cd5016d743f4b15afc09367f",
+    "url": "/paint-run2/public/index.html"
+  },
+  {
+    "revision": "5030216ffb3f74278685",
+    "url": "/paint-run2/public/static/css/main.ad8bf03a.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/paint-run2/public/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "5030216ffb3f74278685",
+    "url": "/paint-run2/public/static/js/main.84ce90b5.chunk.js"
+  },
+  {
+    "revision": "14d7350d33d7f78e64c3",
+    "url": "/paint-run2/public/static/js/runtime~main.4db7d57d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+
+importScripts(
+  "/paint-run2/public/precache-manifest.26ed899682d1335a650dc0110dc4f767.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paint-run2/public/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
